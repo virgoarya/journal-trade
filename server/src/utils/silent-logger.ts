98d8a6bc6b614b114fe29942f7b2f5226c8ff0f3
virgoarya <virga.ariyanto@gmail.com>
2026-07-21 22:27:29 +0700

    fix(log): restore debug signature to accept args but discard silently

--- server/src/utils/silent-logger.ts
+++ server/src/utils/silent-logger.ts
@@ -1,7 +1,7 @@
 import { logger } from "./logger";
 
 export const silentLogger = {
-  debug: () => {
+  debug: (_msg?: any, ..._args: any[]) => {
     // Silently discard debug logs to reduce backend noise
   },
   info: (msg?: any, ...args: any[]) => {
